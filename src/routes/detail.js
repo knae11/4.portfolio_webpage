3eccf1371c78727f1f2ca35c8ad90af09c4517af
Naeun <knae11@naver.com>
2020-09-08 13:45:45 +0900

    structure done'

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { projects } from "../data/projectContents.json";
+import { render } from "@testing-library/react";
 
 class Detail extends React.Component {
-  componentDidMount() {
-    console.log(this.props);
-  }
   render() {
-    return <div>this is detail page I will create.</div>;
+    const { match } = this.props;
+    const projectdetail = projects.find(
+      (project) => project.id === match.params.id
+    );
+    return (
+      <>
+        <section className="info_short">
+          <img
+            style={{ width: "200px", height: "200px" }}
+            src={projectdetail.imgpath}
+            alt={projectdetail.title}
+          />
+          <li>
+            <h3>title: {projectdetail.title}</h3>
+            <h5>skills: {projectdetail.skills}</h5>
+            <h5>description: {projectdetail.description}</h5>
+          </li>
+        </section>
+        <section></section>
+      </>
+    );
   }
 }
-
 export default Detail;
