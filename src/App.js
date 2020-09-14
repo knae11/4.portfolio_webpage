ffe8b0c86c036553ecede3064b14bd57738452e2
Naeun <knae11@naver.com>
2020-09-14 18:30:29 +0900

    version3

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Route path="/projects" exact component={Projects} />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/others" component={Others} />
-      <Route path="/projects/:id" component={Detail} />
+      <Route path="/4.portfolio_webpage/projects" exact component={Projects} />
+      <Route path="/4.portfolio_webpage" exact={true} component={Home} />
+      <Route path="/4.portfolio_webpage/profile" component={Profile} />
+      <Route path="/4.portfolio_webpage/others" component={Others} />
+      <Route path="/4.portfolio_webpage/projects/:id" component={Detail} />
     </BrowserRouter>
   );
 }
