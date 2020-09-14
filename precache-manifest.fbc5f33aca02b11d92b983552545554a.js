6825edf4bfbed9de969352dde67acbafd9ad3d0d
Naeun <knae11@naver.com>
2020-09-14 18:23:54 +0900

    Updates

---
+++ precache-manifest.fbc5f33aca02b11d92b983552545554a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be93671928d157fd5027d3c34dc08af9",
+    "url": "/4.portfolio_webpage/index.html"
+  },
+  {
+    "revision": "9ce46e829519f34fae6b",
+    "url": "/4.portfolio_webpage/static/css/main.be3dd09a.chunk.css"
+  },
+  {
+    "revision": "4287babee9aa06f99036",
+    "url": "/4.portfolio_webpage/static/js/2.3cf4072f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/4.portfolio_webpage/static/js/2.3cf4072f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce46e829519f34fae6b",
+    "url": "/4.portfolio_webpage/static/js/main.ade38638.chunk.js"
+  },
+  {
+    "revision": "502e02ed2289740ef9fa",
+    "url": "/4.portfolio_webpage/static/js/runtime-main.995eecf9.js"
+  }
+]);
